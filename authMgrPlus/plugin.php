05d806fc00c4490791aa6753bb0aec28b1c287f6
Joshua P Panter <joshu@unfettered.net>
2019-11-23 06:07:57 -0500

    hide plugin menu from unpriviledged users #12

--- authMgrPlus/plugin.php
+++ authMgrPlus/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Auth Manager Plus
 Plugin URI:  https://github.com/joshp23/YOURLS-AuthMgrPlus
 Description: Role Based Access Controlls with seperated user data for authenticated users
-Version:     2.0.0
+Version:     2.1.0
 Author:      Josh Panter, nicwaller, Ian Barber <ian.barber@gmail.com>
 Author URI:  https://unfettered.net
 */
@@ -166,10 +166,18 @@ function amp_admin_sublinks( $links ) {
 				unset($links['plugins'][$link]);
 		}
 	}
+
+	if( empty( $links['plugins'] ) )
+		yourls_add_filter( 'admin_links', 'amp_admin_links' );
+		
 	sort($links['plugins']);
 	return $links;
 }
 
+function amp_admin_links( $links ) {
+	unset($links['plugins']);
+	return $links;
+}
 /*
  * Cosmetic filter: displays currently available roles
  * by hovering mouse over the username in logout link.
