d108d422907e786acc09d1ae7375cbfab6674242
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2020-06-24 18:16:08 +0200

    Bumped version number

--- authMgrPlus/plugin.php
+++ authMgrPlus/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Auth Manager Plus
 Plugin URI:  https://github.com/joshp23/YOURLS-AuthMgrPlus
 Description: Role Based Access Controlls with seperated user data for authenticated users
-Version:     2.1.2
+Version:     2.2.0
 Author:      Josh Panter, nicwaller, Ian Barber <ian.barber@gmail.com>
 Author URI:  https://unfettered.net
 */
